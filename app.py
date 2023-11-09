bd51b6a4c572bd1519156a8c3aa89ea54475f302
saturncloudapp1 <149740534+saturncloudapp1@users.noreply.github.com>
2023-11-09 18:07:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify
 
-app = Flask(__name)
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return jsonify({"message": "Hello, World!"})
+     return jsonify({"message": "Hello, World!"})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__=='__main__':
+    app.run(port=8000)  # Defina a porta 8000 aqui
 
